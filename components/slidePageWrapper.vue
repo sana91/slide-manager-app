e8ede84fa4635cfe30e7cf7dea05fa356ca71f5f
sana91 <g.a.513realfield@gmail.com>
2025-11-09 20:19:57 +0900

    過剰な親切

--- components/slidePageWrapper.vue
+++ components/slidePageWrapper.vue
@@ -57,6 +57,47 @@
     >
       <slot></slot>
     </div>
+
+    <div class="mt-8 flex justify-start items-center">
+      <div>
+        <NuxtLink
+          v-if="currentPage > 1"
+          :to="`/slide/${slideId}/${currentPage - 1}`"
+          class="inline-block bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg transition-colors"
+        >
+          ← 前頁
+        </NuxtLink>
+        <div
+          v-else
+          class="inline-block bg-gray-800 text-white px-6 py-3 rounded-lg transition-colors"
+        >
+          ← 前頁
+        </div>
+      </div>
+
+      <div
+        class="inline-block bg-gray-900 text-white px-6 py-3 rounded-lg transition-colors text-center"
+      >
+        <span> [ {{ currentPage }} / {{ totalPages }} ]</span>
+      </div>
+
+      <div>
+        <NuxtLink
+          v-if="currentPage < totalPages"
+          :to="`/slide/${slideId}/${currentPage + 1}`"
+          class="inline-block bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg transition-colors"
+        >
+          次頁 →
+        </NuxtLink>
+        <div
+          v-else
+          class="inline-block bg-gray-800 text-white px-6 py-3 rounded-lg transition-colors"
+        >
+          次頁 →
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
